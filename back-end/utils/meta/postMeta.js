dc64d15a7f1e8111d9d976d8569d07e401c0cc56
LeDimas <morph232@inbox.lv>
2021-08-27 02:54:33 +0300

    post metadata file small fix

--- back-end/utils/meta/postMeta.js
+++ back-end/utils/meta/postMeta.js
@@ -13,10 +13,10 @@ function addPostMeta (req, res, initialPosts) {
     
     //filter meta posts
     meta.filter = {}
-    meta.filter.publicationDate =  `${process.env.API_URL}${baseUrl}${path === '/'?'':path}?offset=${offset}&limit=${limit}&filter=publicationDate` 
+    meta.filter.publicationDate =  `${process.env.API_URL}${baseUrl}${path === '/'?'':path.replace(/\/\d+$/ , '')}?offset=${offset}&limit=${limit}&filter=publicationDate` 
     
     meta.order = {}
-    meta.order.publicationDate =  `${process.env.API_URL}${baseUrl}${path === '/'?'':path}?offset=${offset}&limit=${limit}&order=publicationDate` 
+    meta.order.publicationDate =  `${process.env.API_URL}${baseUrl}${path === '/'?'':path.replace(/\/\d+$/ , '')}?offset=${offset}&limit=${limit}&order=publicationDate` 
     
     //not best idea
     if(path !== '/userPosts'){
@@ -28,23 +28,23 @@ function addPostMeta (req, res, initialPosts) {
     const isList = Array.isArray(initialPosts)
 
     if(isList){
-        meta.single_post = `${process.env.API_URL}${baseUrl}${path === '/'?'':path}${path === '/'?'':path}/:id`
+        meta.single_post = `${process.env.API_URL}${baseUrl}${path === '/'?'':path.replace(/\/\d+$/ , '')}/:id`
 
         if(initialPosts.length > limit){
             meta.links = {}
 
             if(offset > 0 ){
-                meta.links.first = `${process.env.API_URL}${baseUrl}${path === '/'?'':path}?offset=0&limit=${limit}`
+                meta.links.first = `${process.env.API_URL}${baseUrl}${path === '/'?'':path.replace(/\/\d+$/ , '')}?offset=0&limit=${limit}`
 
-                meta.links.prev = `${process.env.API_URL}${baseUrl}${path === '/'?'':path}?offset=${Math.max(offset - limit , 0)}&limit=${limit}`
+                meta.links.prev = `${process.env.API_URL}${baseUrl}${path === '/'?'':path.replace(/\/\d+$/ , '')}?offset=${Math.max(offset - limit , 0)}&limit=${limit}`
             
             }
             
             if(initialPosts.length > offset + limit){
-                meta.links.next = `${process.env.API_URL}${baseUrl}${path === '/'?'':path}?offset=${offset + limit}&limit=${limit}`
+                meta.links.next = `${process.env.API_URL}${baseUrl}${path === '/'?'':path.replace(/\/\d+$/ , '')}?offset=${offset + limit}&limit=${limit}`
 
                 const lastPossibleOffset = Math.floor((initialPosts.length - 1) / limit) * limit
-                meta.links.last = `${process.env.API_URL}${baseUrl}${path === '/'?'':path}?offset=${lastPossibleOffset}&limit=${limit}`
+                meta.links.last = `${process.env.API_URL}${baseUrl}${path === '/'?'':path.replace(/\/\d+$/ , '')}?offset=${lastPossibleOffset}&limit=${limit}`
             }
 
             posts = initialPosts.slice(offset , offset + limit)
