813d32cc9b2942bd165bc727839602c48e3414e1
Arthur <jermolickiy@gmail.com>
2021-09-09 23:44:58 +0300

    am

--- vue/src/http-common.js
+++ vue/src/http-common.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:8080/api",
-  // baseURL: "http://nodevueacc.herokuapp.com/api",
-  baseURL: "api",
+  baseURL: "http://nodevueacc.herokuapp.com/api",
+  //baseURL: "api",
   headers: {
     "Content-type": "application/json"
   }
