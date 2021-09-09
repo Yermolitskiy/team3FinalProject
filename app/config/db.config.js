6441f6c17395537e500f722bc42cd7cc1c60e22d
Arthur <jermolickiy@gmail.com>
2021-09-09 08:14:05 +0300

    ms

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,7 +2,6 @@ module.exports = {
     HOST: "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     USER: "t1q3vjb0lto07koo",
     PASSWORD: "s4huvkmdne1kunn9",
-    // PORT: "3306", //?
     DB: "ivxkhwhpbwrj4z1z",
     dialect: "mysql",
     pool: {
