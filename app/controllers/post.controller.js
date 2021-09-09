a832d0aac4410a22c8137681c02ad2e117d1ee1e
Arthur <jermolickiy@gmail.com>
2021-09-09 20:04:33 +0300

    am

--- app/controllers/post.controller.js
+++ app/controllers/post.controller.js
@@ -53,10 +53,8 @@ exports.create = (req, res) => {
       title: req.body.title,
       description: req.body.description,
       published: req.body.published ? req.body.published : false,
-      imageUrl: req.file ? `http://localhost:8080/public/uploads/posts/${req.file.filename}` : false,//`http://localhost:8080/public/uploads/posts/${req.file.filename}` : false,
+      imageUrl: req.file ? `http://https://nodevueacc.herokuapp.com:2502/public/uploads/posts/${req.file.filename}` : false,//`http://localhost:8080/public/uploads/posts/${req.file.filename}` : false, //frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306
       // imageUrl: req.body.file ? `${process.env.API_URL}${process.env.PUBLIC_POSTS_STATIC}${req.file.filename}`,
-
-      
     };
   
     // Save post in the database
