c1b1c8428605fb0bdafcfa44395cf1bf4c1d782e
Arthur <jermolickiy@gmail.com>
2021-09-09 08:44:20 +0300

    am

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,13 @@
-module.exports = {
-  production: {
-    HOST: "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    USER: "t1q3vjb0lto07koo",
-    PASSWORD: "s4huvkmdne1kunn9",
-    DB: "ivxkhwhpbwrj4z1z",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  }
+module.exports = {  
+  HOST: "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  USER: "t1q3vjb0lto07koo",
+  PASSWORD: "s4huvkmdne1kunn9",
+  DB: "ivxkhwhpbwrj4z1z",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }  
 };
