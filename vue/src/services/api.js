d657ce2d25d1a13e384aaf28ea8f65d8f5baa539
Arthur <jermolickiy@gmail.com>
2021-09-10 10:04:15 +0300

    am

--- vue/src/services/api.js
+++ vue/src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:8080/api", // for development environment
-  // baseURL: "/api", // for production environment
-  baseURL: "https://nodevueacc.herokuapp.com/api",
+  baseURL: "/api", // for production environment
+  // baseURL: "https://nodevueacc.herokuapp.com/api",
   headers: {
     "Content-Type": "application/json",
   },
